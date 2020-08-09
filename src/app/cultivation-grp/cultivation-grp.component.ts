fa4862372d76796c6aed7e99c470e5f4eff9e877
joshyjoy <joshy.joy@cognicor.com>
2020-08-09 12:38:52 +0530

    groupby removed from cultivation group component and style changes applied

--- src/app/cultivation-grp/cultivation-grp.component.ts
+++ src/app/cultivation-grp/cultivation-grp.component.ts
@@ -13,14 +13,8 @@ export class CultivationGrpComponent implements OnInit {
   startdateCmp: any;
   enddateCmp: any;
   displayedColumns: any;
-  displayedColumnsCmp: any;
   dataSource: any;
   dataSourceCmp: any;
-  showCompare: boolean;
-  selected: string;
-  selectedCmp: string;
-  group: string;
-  groupCmp: string;
 
   constructor(private http: HttpClient) {
   }
@@ -31,49 +25,25 @@ export class CultivationGrpComponent implements OnInit {
 	this.startdate.setDate(this.startdate.getDate() - 1);
 	this.startdateCmp = this.startdate;
 	this.enddateCmp = this.enddate;
-	this.showCompare = false;
 	this.displayedColumns = ['Job_Name', 'Mandays', 'AreaCovered', 'MndArea'];
-	this.displayedColumnsCmp = ['Job_Name', 'Mandays', 'AreaCovered', 'MndArea'];
-	this.selected = 'job';
-	this.selectedCmp = 'job';
-	this.group = 'job';
-	this.groupCmp = 'job';
 
-	const url = 'http://127.0.0.1:5000/cultgroup?start=' + this.convert(this.startdate) + '&end=' + this.convert(this.enddate) + '&grpby=' + this.selected;
+	const url = 'http://127.0.0.1:5000/cultgroup?start=' + this.convert(this.startdate) + '&end=' + this.convert(this.enddate);
 	this.http.get(url).subscribe((data: CultivationGroupByJob) => {
 		this.dataSource = data;
 		});
 	}
 
 	clickedGo() {
-		const url = 'http://127.0.0.1:5000/cultgroup?start=' + this.convert(this.startdate) + '&end=' + this.convert(this.enddate) + '&grpby=' + this.selected;
-		this.http.get(url).subscribe((data: any) => {
-			this.group = this.selected;
-			if (this.selected === 'job') {
-				this.displayedColumns = ['Job_Name', 'Mandays', 'AreaCovered', 'MndArea'];
-				this.parseJobData('report', data);
-			} else {
-				this.displayedColumns = ['Section_Name', 'Mandays', 'AreaCovered', 'MndArea'];
-				this.parseSectionData('report', data);
-			}
+		const url = 'http://127.0.0.1:5000/cultgroup?start=' + this.convert(this.startdate) + '&end=' + this.convert(this.enddate);
+		this.http.get(url).subscribe((data: CultivationGroupByJob) => {
+		this.dataSource = data;
 		});
 	}
 
-	clickedCompare() {
-		this.showCompare = true;
-	}
-
 	clickedGoCompare() {
-		const url = 'http://127.0.0.1:5000/cultgroup?start=' + this.convert(this.startdateCmp) + '&end=' + this.convert(this.enddateCmp) + '&grpby=' + this.selectedCmp;
-		this.http.get(url).subscribe((data: any) => {
-			this.groupCmp = this.selectedCmp;
-			if (this.selectedCmp === 'job') {
-				this.displayedColumnsCmp = ['Job_Name', 'Mandays', 'AreaCovered', 'MndArea'];
-				this.parseJobData('compare', data);
-			} else {
-				this.displayedColumnsCmp = ['Section_Name', 'Mandays', 'AreaCovered', 'MndArea'];
-				this.parseSectionData('compare', data);
-			}
+		const url = 'http://127.0.0.1:5000/cultgroup?start=' + this.convert(this.startdateCmp) + '&end=' + this.convert(this.enddateCmp);
+		this.http.get(url).subscribe((data: CultivationGroupByJob) => {
+			this.dataSourceCmp = data;
 		});
 	}
 
@@ -93,31 +63,17 @@ export class CultivationGrpComponent implements OnInit {
 		return [date.getFullYear(), mnth, day].join("-").toString();
 	}
 
-	parseJobData(type: string, data: CultivationGroupByJob) {
-		switch (type) {
-			case 'report': this.dataSource = data; break;
-			case 'compare': this.dataSourceCmp = data; break;
+	getTotal(_dataSrc: string, _field: string) {
+		if (this[_dataSrc]) {
+			return this[_dataSrc].map(t => t[_field]).reduce((acc, value) => acc + value, 0);
+		} else {
+			return null;
 		}
 	}
-
-	parseSectionData(type: string, data: CultivationGroupBySection) {
-		switch (type) {
-			case 'report': this.dataSource = data; break;
-			case 'compare': this.dataSourceCmp = data; break;
-		}
-	}
-
 }
 
 export interface CultivationGroupByJob {
 	Job_Name: string;
-	Mandays: number;
-	AreaCovered: number;
-	MndArea: number;
-}
-
-export interface CultivationGroupBySection {
-	Section_Name: string;
 	Mandays: number;
 	AreaCovered: number;
 	MndArea: number;
